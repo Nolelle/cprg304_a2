16865d40a2cc40cf79c2c6f092f3780e6e76c0ec
Edmund Yu <38992135+Nolelle@users.noreply.github.com>
2024-11-22 01:07:59 -0700

    working on dll and arraylist implementations

--- src/implementations/MyDLL.java
+++ src/implementations/MyDLL.java
@@ -1,5 +1,7 @@
 package implementations;
 
+import java.util.Iterator;
+
 import utilities.ListADT;
 
 public class MyDLL<E> implements ListADT<E> {
@@ -15,7 +17,7 @@ public class MyDLL<E> implements ListADT<E> {
 		}
 	}
 
-	private Node head;
+	private Node head, tail;
 	private int size;
 
 	@Override
@@ -32,6 +34,116 @@ public class MyDLL<E> implements ListADT<E> {
 
 	@Override
 	public boolean add(E toAdd) {
+		if (toAdd == null) {
+			throw new NullPointerException();
+		}
+		Node newNode = new Node(toAdd);
+		if (size == 0) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			newNode.prev = tail;
+			tail = newNode;
+		}
+		size++;
+		return true;
 	}
 
+	@Override
+	public boolean add(int index, E toAdd) {
+		if (toAdd == null) {
+			throw new NullPointerException();
+		}
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (index == size) {
+			return add(toAdd);
+		}
+		Node newNode = new Node(toAdd);
+		if (index == 0) {
+			newNode.next = head;
+			head.prev = newNode;
+			head = newNode;
+		} else {
+			Node current = getNodeAt(index);
+			newNode.next = current;
+			newNode.prev = current.prev;
+			if (current.prev != null) {
+				current.prev.next = newNode;
+				current.prev = newNode;
+			}
+		}
+		size++;
+		return true;
+	}
+
+	@Override
+	public E get(int index) {
+		return getNodeAt(index).data;
+	}
+
+	@Override
+
+	public E remove(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		Node current = getNodeAt(index);
+		if (size == 1) {
+			head = null;
+			tail = null;
+		} else if (index == 0) {
+			head = head.next;
+			head.prev = null;
+		} else if (index == size - 1) {
+			tail = tail.prev;
+			tail.next = null;
+		} else {
+			current.prev.next = current.next;
+			current.next.prev = current.prev;
+		}
+		size--;
+		return current.data;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new Iterator<E>() {
+			private Node current = head;
+
+			@Override
+			public boolean hasNext() {
+				return current != null;
+			}
+
+			@Override
+			public E next() {
+				if (!hasNext()) {
+					throw new IndexOutOfBoundsException();
+				}
+				E data = current.data;
+				current = current.next;
+				return data;
+			}
+		};
+	}
+
+	private Node getNodeAt(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		Node current = head;
+		for (int i = 0; i < index; i++) {
+			current = current.next;
+		}
+		return current;
+	}
 }
